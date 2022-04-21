16e78e1a5f788e24e89552348e10a3da9e5c4bcb
Alexandre Ratel <alexandre.ratel@etu.unilim.fr>
2022-04-22 01:59:38 +0200

    add Player class

---
+++ src/main/java/latice/application/model/Player.java
@@ -0,0 +1,20 @@
+package latice.application.model;
+
+public class Player {
+	private String username;
+	private Integer score;
+	private Integer remainingTiles;
+	private Rack rack;
+	
+	private void changeTile(final Integer tileNum) {
+		//TODO
+	}
+	
+	private void changeRack() {
+		//TODO
+	}
+	
+	private void placeTile() {
+		//TODO
+	}
+}
