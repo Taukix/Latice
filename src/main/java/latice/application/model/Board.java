326bb82c08379e13c8c69405494d7fa78d028042
Alexandre Ratel <alexandre.ratel@etu.unilim.fr>
2022-04-22 02:43:30 +0200

    Add in comments a possible solution for the constructor

--- src/main/java/latice/application/model/Board.java
+++ src/main/java/latice/application/model/Board.java
@@ -1,11 +1,15 @@
 package latice.application.model;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Board {
 	public ArrayList<ArrayList<Cell>> cells;
 	
 	private Board() {
 		//TODO
+		// Need to find the right formula to find bonus cells
+		// First guess would be : i + j % 2 && i != 3 && i !=5 && j != 3 && j !=5 && i + j != 6
+		// but need confirmation
 	}
 }
