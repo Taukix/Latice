7b07849828780c8912b0b137d33d9642a8d3ef20
Alexandre Ratel <alexandre.ratel@etu.unilim.fr>
2022-04-22 02:12:42 +0200

    add Cell class attributes and constructor

--- src/main/java/latice/application/model/Cell.java
+++ src/main/java/latice/application/model/Cell.java
@@ -4,6 +4,13 @@ public class Cell {
 	private Tile tile;
 	private Boolean bonus;
 	
+	
+	// Par défaut une cellule du plateau n'a pas de tuile
+	private Cell(Boolean bonus) {
+		this.tile = null;
+		this.bonus = bonus;
+	}
+	
 	private Boolean isEmpty() {
 		return tile.equals(null);
 	}
