3b6f86d4e18fbfed1c18ecbab02acba3ba603bf8
Alexandre Ratel <alexandre.ratel@etu.unilim.fr>
2022-04-22 02:38:42 +0200

    Add gameTileList attribute + add constructor

--- src/main/java/latice/application/model/Game.java
+++ src/main/java/latice/application/model/Game.java
@@ -1,8 +1,16 @@
 package latice.application.model;
 
+import java.util.ArrayList;
+
 public class Game {
 	private Player player1;
 	private Player player2;
 	private Board board;
+	private ArrayList<Tile> gameTileList;
 	
+	private Game(Player p1, Player p2) {
+		this.player1 = p1;
+		this.player2 = p2;
+		Board = new Board();
+	}
 }
