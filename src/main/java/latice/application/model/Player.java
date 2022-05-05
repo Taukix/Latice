575a4389c207c8a186b078310bc2c309c284734b
Alexandre Ratel <alexandre.ratel@etu.unilim.fr>
2022-05-05 11:40:23 +0200

    Add constructor

--- src/main/java/latice/application/model/Player.java
+++ src/main/java/latice/application/model/Player.java
@@ -1,10 +1,20 @@
 package latice.application.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Player {
 	private String username;
 	private Integer score;
-	private Integer remainingTiles;
+	private Map<Integer, Tile> stack;
 	private Rack rack;
+	
+	public Player(String username){
+		this.username = username;
+		this.score = 0;
+		this.stack = new HashMap<>();
+		this.rack = new Rack();
+	}
 	
 	private void changeTile(final Integer tileNum) {
 		//TODO
