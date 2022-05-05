07e6d61fa7658942e5bef29e03432b76fb8290a2
Alexandre Ratel <alexandre.ratel@etu.unilim.fr>
2022-05-05 11:33:44 +0200

    Add constructor

--- src/main/java/latice/application/model/Tile.java
+++ src/main/java/latice/application/model/Tile.java
@@ -1,6 +1,18 @@
 package latice.application.model;
 
 public class Tile {
+	@Override
+	public String toString() {
+		return "Tile [color=" + color + ", shape=" + shape + "]";
+	}
+
 	private Color color;
 	private Shape shape;
+	
+	public Tile(Color color, Shape shape) {
+		this.color = color;
+		this.shape = shape;
+	}
+	
+	
 }
