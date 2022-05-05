f4dd21b5cf2d289a55ecca0e9a9c989653904799
Alexandre Ratel <alexandre.ratel@etu.unilim.fr>
2022-05-05 11:33:30 +0200

    Add Position class

---
+++ src/main/java/latice/application/model/Position.java
@@ -0,0 +1,22 @@
+package latice.application.model;
+
+public class Position {
+	private Integer x;
+	private Integer y;
+	
+	
+	public Position(final Integer x, final Integer y){
+		this.x = x;
+		this.y = y;
+	}
+
+
+	public Integer x() {
+		return x;
+	}
+
+
+	public Integer y() {
+		return y;
+	}
+}
