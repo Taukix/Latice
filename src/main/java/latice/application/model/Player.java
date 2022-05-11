4b3b7da86f55d59cf1ad551272b6350c864a5725
Alexandre Ratel <alexandre.ratel@etu.unilim.fr>
2022-05-11 23:43:30 +0200

    (WIP) add placeTile

--- src/main/java/latice/application/model/Player.java
+++ src/main/java/latice/application/model/Player.java
@@ -1,5 +1,6 @@
 package latice.application.model;
 
+import java.io.Console;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,8 +24,21 @@ public class Player {
 		//TODO
 	}
 	
-	public void placeTile() {
-		//TODO
+	public void placeTile(Game game, int x, int y, int tileOfRack) {
+		Position inGamePos = new Position(x, y);
+		Tile tile = rack.getTiles().get(tileOfRack);
+		if(!tile.equals(null)) {
+			if(x > 0 && x <= Constants.BOARD_SIZE && y > 0 && y <= Constants.BOARD_SIZE) {				
+				Boolean bonus;
+				//Check if bonus is in the cross of the map
+				bonus = ((x == y) || (x == y - Constants.BOARD_SIZE)) && (x <= 3 || x >= 7) && (y <= 3 || y >= 7);
+				
+				//Check if bonus is in on the border of the map
+				bonus = bonus || (y == 4 && (x == 1 || x == 9)) || (x == 4 && (y == 1 || y == 9)) ;
+				
+				game.getBoard().cells.put(inGamePos, new Cell(tile, bonus));
+			}
+		}
 	}
 
 	public void addTileToStack(final Tile tile){
