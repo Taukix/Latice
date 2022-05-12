fc97cef4dd1558121222464edb48daa007b8eddd
Alexandre Ratel <alexandre.ratel@etu.unilim.fr>
2022-05-12 17:09:32 +0200

    (WIP) Add turn management

--- src/main/java/latice/application/model/Player.java
+++ src/main/java/latice/application/model/Player.java
@@ -7,6 +7,8 @@ import java.util.List;
 public class Player {
 	public String username;
 	public Integer score;
+	public boolean consumedTurn;
+	public boolean turn;
 	public List<Tile> stack;
 	public Rack rack;
 	
@@ -14,6 +16,7 @@ public class Player {
 		this.username = username;
 		this.score = 0;
 		this.stack = new ArrayList<>();
+		turn = false;
 	}
 	
 	public void changeTile(final Integer tileNum) {
@@ -23,23 +26,55 @@ public class Player {
 	public void changeRack() {
 		//TODO
 	}
-	
-	public void placeTile(Game game, Position pos, int tileOfRack) {
+
+
+	public boolean placeTile(Game game, Position pos, int tileOfRack) {
+		boolean inMap = false;
+		if(turn) {
 		int x = pos.x();
 		int y = pos.y();
-		Tile tile = rack.getTiles().get(tileOfRack);
+		Tile tile = rack.getTiles().remove(tileOfRack);
+		
 		if(!tile.equals(null)) {
-			if(x > 0 && x <= Constants.BOARD_SIZE && y > 0 && y <= Constants.BOARD_SIZE) {				
+			if(x > 0 && x <= Constants.BOARD_SIZE && y > 0 && y <= Constants.BOARD_SIZE && (!consumedTurn|| score > 2)) {				
 				Boolean bonus;
+				if(consumedTurn) {
+					score = score - 2;
+				}
 				//Check if bonus is in the cross of the map
 				bonus = ((x == y) || (x == y - Constants.BOARD_SIZE)) && (x <= 3 || x >= 7) && (y <= 3 || y >= 7);
 				
 				//Check if bonus is in on the border of the map
 				bonus = bonus || (y == 4 && (x == 1 || x == 9)) || (x == 4 && (y == 1 || y == 9)) ;
 				
-				game.getBoard().cells.put(pos, new Cell(tile, bonus));
+				game.getBoard().tiles.put(pos, tile);
+				if(bonus && score%2 < 3) {
+					score += 2;
+				}
+				consumedTurn = true;
+				inMap = true;
 			}
 		}
+		else {
+			rack.getTiles().add(tile);
+		}
+		}
+		return inMap;
+	}
+	
+	public void endTurn() {
+		refreshRack();
+		consumedTurn = true;
+		turn = false;
+	}
+	
+	public void startTurn() {
+		consumedTurn = false;
+		turn = true;
+	}
+	
+	public void refreshRack() {
+		this.rack.fillRackWithTiles(stack);
 	}
 
 	public void addTileToStack(final Tile tile){
@@ -48,7 +83,7 @@ public class Player {
 	
 	public void generateRack() {
 		this.rack = new Rack();
-		this.rack.FillRack(stack);
+		this.rack.fillRackWithTiles(stack);
 	}
 	
 	public Integer countTilesInStack() {
