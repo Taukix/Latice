57a04ccea524cf3c4095cb960b73d1e23dca07d3
Alexandre Ratel <alexandre.ratel@etu.unilim.fr>
2022-05-21 02:04:41 +0200

    add CENTER Position to Constants

--- src/main/java/latice/application/model/Constants.java
+++ src/main/java/latice/application/model/Constants.java
@@ -3,4 +3,5 @@ package latice.application.model;
 public final class Constants {
 	public static final int BOARD_SIZE = 9;
 	public static final int RACK_SIZE = 5;
+	public static final Position CENTER = new Position(5,5);
 }
