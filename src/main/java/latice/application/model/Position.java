28aff926a7f827f65a538a2667fd8ef8e0ebb7d2
Alexandre Ratel <alexandre.ratel@etu.unilim.fr>
2022-05-21 02:05:58 +0200

    add .equals() and .hashCode()

--- src/main/java/latice/application/model/Position.java
+++ src/main/java/latice/application/model/Position.java
@@ -1,6 +1,27 @@
 package latice.application.model;
 
+import java.util.Objects;
+
 public class Position {
+	@Override
+	public int hashCode() {
+		return Objects.hash(x, y);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Position other = (Position) obj;
+		return Objects.equals(x, other.x) && Objects.equals(y, other.y);
+	}
+
+
 	private Integer x;
 	private Integer y;
 	
@@ -8,6 +29,12 @@ public class Position {
 	public Position(final Integer x, final Integer y){
 		this.x = x;
 		this.y = y;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Position [x=" + x + ", y=" + y + "]";
 	}
 
 
