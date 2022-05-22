88478f3a8ed078f1213dabad80ae7241b9099987
Alexandre Ratel <alexandre.ratel@etu.unilim.fr>
2022-05-23 01:05:56 +0200

    add PlayerTest test class

---
+++ src/test/java/latice/application/model/PlayerTest.java
@@ -0,0 +1,14 @@
+package latice.application.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PlayerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
