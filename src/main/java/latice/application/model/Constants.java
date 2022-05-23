ca6e74ab5a4a7584b680d703dfb03ab8e44a4a88
Alexandre Ratel <alexandre.ratel@etu.unilim.fr>
2022-05-23 02:06:00 +0200

    add new global variable : MAX_BONUS

--- src/main/java/latice/application/model/Constants.java
+++ src/main/java/latice/application/model/Constants.java
@@ -4,4 +4,5 @@ public final class Constants {
 	public static final int BOARD_SIZE = 9;
 	public static final int RACK_SIZE = 5;
 	public static final Position CENTER = new Position(5,5);
+	public static final int MAX_BONUS = 6;
 }
