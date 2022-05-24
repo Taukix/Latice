764dfe15e2b5394a99769466bb26b40c2803b072
Alexandre Ratel <alexandre.ratel@etu.unilim.fr>
2022-05-24 10:20:55 +0200

    removed useless constant

--- src/main/java/latice/application/model/Constants.java
+++ src/main/java/latice/application/model/Constants.java
@@ -4,5 +4,4 @@ public final class Constants {
 	public static final int BOARD_SIZE = 9;
 	public static final int RACK_SIZE = 5;
 	public static final Position CENTER = new Position(5,5);
-	public static final int MAX_BONUS = 6;
 }
